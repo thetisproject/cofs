112017e9bebea365c418fb13c35d4f8b117f0951
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2020-03-19 12:43:58 +0000

    Testing infrastructure for lagrangian vs eulerian

--- test/mesh_movement/test_lagrangian_vs_eulerian.py
+++ test/mesh_movement/test_lagrangian_vs_eulerian.py
@@ -1,13 +1,16 @@
+"""
+Solve a simple tracer advection problem in a singly periodic domain with constant uniform fluid
+velocity parallel to the mesh periodicity.
+
+In Lagrangian mode, the mesh should be identical to how it was initially after one cycle.
+"""
 from thetis import *
+import pytest
 
-def solve_periodic(mode):
-    """
-    Solve a simple tracer advection problem with constant uniform fluid velocity parallel to the
-    mesh periodicity.
 
-    In Lagrangian mode, the mesh should be identical to how it was initially after one cycle.
-    """
+def run(mode, **model_options):
     assert mode in ('lagrangian', 'eulerian')
+    print_output("--- running {:s} mode".format(mode.capitalize()))
 
     # Set up domain
     n, L, T = 40, 10.0, 10.0
@@ -17,7 +20,6 @@ def solve_periodic(mode):
     # Create solver object
     solver_obj = solver2d.FlowSolver2d(mesh2d, Constant(1.0))
     options = solver_obj.options
-    options.timestepper_type = 'CrankNicolson'
     options.timestep = 0.2
     options.simulation_export_time = T/10
     options.simulation_end_time = T
@@ -27,6 +29,7 @@ def solve_periodic(mode):
     options.tracer_only = True
     options.horizontal_diffusivity = Constant(0.0)
     options.use_lagrangian_formulation = mode == 'lagrangian'
+    options.update(model_options)
 
     # Constant uniform fluid velocity parallel to mesh periodicity
     fluid_velocity = Constant(as_vector([L/T, 0.0]))
@@ -54,10 +57,24 @@ def solve_periodic(mode):
             assert final_coords.vector().gather().min() > -tol
         except AssertionError:
             raise ValueError("Initial and final mesh coordinates do not match.")
-    msg = "Relative error in '{:s}' mode: {:.4f}%"
-    print_output(msg.format(mode, 100*abs(errornorm(init, final)/norm(init))))
+    error = abs(errornorm(init, final)/norm(init))
+    print_output("Relative error in {:s} mode: {:.4f}%".format(mode.capitalize(), 100*error))
+    return error
 
+# ---------------------------
+# standard tests for pytest
+# ---------------------------
+
+@pytest.mark.parametrize(('stepper'),
+                         [('CrankNicolson')])
+def test_lagrangian_vs_eulerian(stepper):
+    eulerian_error = run('eulerian', timestepper_type=stepper)
+    lagrangian_error = run('lagrangian', timestepper_type=stepper)
+    assert lagrangian_error < eulerian_error
+
+# ---------------------------
+# run individual setup for debugging
+# ---------------------------
 
 if __name__ == "__main__":
-    solve_periodic('lagrangian')
-    # solve_periodic('eulerian')
+    run('lagrangian')
